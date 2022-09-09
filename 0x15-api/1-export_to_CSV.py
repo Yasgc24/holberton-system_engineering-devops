c814412fb0b84d93148d6898a2ecaece0797721c
Yasgc24 <4537@holbertonschool.com>
2022-09-09 11:00:08 -0500

    Modified task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,27 +3,26 @@
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
     id = argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+    URL_tasks = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
         id)
+    URL_user = "https://jsonplaceholder.typicode.com/users/{}".format(id)
 
     with requests.session() as session:
-        response_tasks = session.get(todos_url)
-        response_users = session.get(user_url)
+        response_tasks = session.get(URL_tasks)
+        response_users = session.get(URL_user)
         data_tasks = response_tasks.json()
         data_users = response_users.json()
         username = data_users["username"]
         tasks_done = 0
-        total_tasks = len(data_tasks)
+        tasks_all = len(data_tasks)
         data = ""
         for record in data_tasks:
             completed = record["completed"]
             title = record["title"]
             data += '"{}","{}","{}","{}"\n'.format(
                 id, username, completed, title)
-        text_file = "{}.cvs".format(id)
-        with open(text_file, mode="w+", encoding="utf-8") as file:
+        textFile = "{}.csv".format(id)
+        with open(textFile, mode="w+", encoding="utf-8") as file:
             file.write(data)
