e9a704aa1d17acb46cd18dda56797e6afedd6b25
Yasgc24 <4537@holbertonschool.com>
2022-09-10 11:34:41 -0500

    pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None, count = 0):
+def recurse(subreddit, hot_list=[], after=None, count=0):
     """queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit"""
     url = "https://www.reddit.com/r/{}.json".format(subreddit)
